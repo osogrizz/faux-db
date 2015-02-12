a9a6b6cba1e5dd58290ebd75be748282698bf3e6
Leo Torres <leo.htor@gmail.com>
2015-02-12 09:40:12 -0800

    initial commit

--- app.rb
+++ app.rb
@@ -25,11 +25,45 @@ get '/' do
 end
 
 # GET /users/new - display a form for making a new user
+
+get '/users/new' do
+  erb :new
+end
+
 #
 # POST /users - create a user based on params from form
+post '/users' do
+  users.push first: params[:first], last: params[:last], id: id 
+  id += 1
+  redirect to '/'
+end
+
 #
 # GET /users/:id - show a user's info by their id, this should display the info in a form
+get '/users/:id' do 
+  erb :edit
+end
+
 #
 # PUT /users/:id - update a user's info based on the form from GET /users/:id
+put '/users/:id' do
+  user = users[params[:id].to_i(":id")  ] # this needs to search the array for the right person
+  user[:first] = params[:first]
+  user[:last] = params[:last]
+  redirect to '/'
+end
+
 #
 # DELETE /users/:id - delete a user by their id
+delete '/users/:id' do
+  users.delete_if{ |key, val| key == "#{user[:id]}" }
+  
+  redirect to '/'
+end
+
+
+
+
+
+
+
